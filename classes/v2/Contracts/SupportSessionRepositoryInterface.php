934af146a4cc74a798a3d0a820509c764130a583
Hendrix Nwaokolo <n.hendrix.e@gmail.com>
2026-08-28 19:15:21 +0100

    feat(v2): define support session repository contract

---
+++ classes/v2/Contracts/SupportSessionRepositoryInterface.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace APP\plugins\generic\chatwootIntegration\classes\v2\Contracts;
+
+use APP\plugins\generic\chatwootIntegration\classes\v2\Session\SupportSession;
+
+interface SupportSessionRepositoryInterface
+{
+    public function create(SupportSession $session): void;
+
+    public function save(SupportSession $session): void;
+
+    public function findByPublicId(string $publicId): ?SupportSession;
+
+    public function findByBindingTokenHash(string $bindingTokenHash): ?SupportSession;
+
+    public function findByConversationBinding(
+        int $contextId,
+        string $chatwootAccountId,
+        string $chatwootContactId,
+        string $chatwootConversationId
+    ): ?SupportSession;
+
+    public function revokeActiveUnboundForUser(int $contextId, int $userId, int $now): void;
+
+    public function purgeExpired(int $now): int;
+}
